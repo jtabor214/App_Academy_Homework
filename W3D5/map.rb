b6ca78a995181a51d669fea48b805cdbe3312d71
Jacob Tabor <jtabor214@gmail.com>
2023-11-03 01:32:44 -0400

    Filled out instance methods #initialize, #set, #get, #delete, #show

--- W3D5/map.rb
+++ W3D5/map.rb
@@ -1,4 +1,39 @@
 class Map 
-    
+
+    def initialize(hash_array)
+        @stack = hash_array
+    end
+
+    def set(key, value)
+        @stack.each do |hash|
+            if hash[0] == key
+                hash[0] = value
+            else
+                hash[key] = value
+            end
+        end
+        @stack
+    end
+
+    def get(key)
+        @stack.each do |hash|
+            if hash[0] == key
+                return hash[0]
+            end
+        end
+    end
+
+    def delete(key)
+        @stacks.each do |hash|
+            if hash[0] == key
+                hash.delete_at(0)
+            end
+        end
+        @stacks
+    end
+
+    def show
+        puts @stacks
+    end
 
 end
