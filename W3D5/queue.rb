25a1f21cbd97ec5c4caacbc0ea00722d8d55c7f5
Jacob Tabor <jtabor214@gmail.com>
2023-11-03 01:11:21 -0400

    Completed #initialize, #enqueue, #dequeue, and #peek for Queue

--- W3D5/queue.rb
+++ W3D5/queue.rb
@@ -1,19 +1,25 @@
 class Queue
 
-    def initialize
-
+    def initialize(array)
+        @stack = array
     end
 
     def enqueue(el)
-
+        @stack.unshift(el)
     end
 
     def dequeue
-
+        @stack.pop
+        return @stack
     end
 
     def peek
-
+        return @stack[0]
     end
 
 end
+
+a = Queue.new([1,2,3,4,5,6])
+p a.dequeue
+p a.enqueue(4)
+p a.peek
