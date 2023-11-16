d9c809a922bd1bb637ad2155ee97a6ba2849fe83
Jacob Tabor <jtabor214@gmail.com>
2023-11-16 00:12:09 -0500

    Created person.rb and house.rb inside app/models

--- W5D4/practice-for-ch-sql-intro-to-rails-main/intro_to_rails/app/models/person.rb
+++ W5D4/practice-for-ch-sql-intro-to-rails-main/intro_to_rails/app/models/person.rb
@@ -1,6 +1,10 @@
 
 
-class Person
+class Person < ApplicationRecord
+    validates :name, :house_id, presence: true
 
-
+    belongs_to :house, 
+      primary_key: :id,
+      foreign_key: :house_id,
+      class_name: :House
 end
